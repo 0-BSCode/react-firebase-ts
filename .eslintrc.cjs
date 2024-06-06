47f3a389e593e1fcb56168854904c72b7bf7f3bb
0-BSCode <bryanipsanchez@gmail.com>
2024-06-06 15:41:58 +0800

    Tweak and test linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,16 +15,16 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
-        "unused-imports/no-unused-imports": "error",
-        "unused-imports/no-unused-vars": [
-            "warn",
-            {
-                "vars": "all",
-                "varsIgnorePattern": "^_",
-                "args": "after-used",
-                "argsIgnorePattern": "^_",
-            },
-        ]
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+        "warn",
+        {
+            "vars": "all",
+            "varsIgnorePattern": "^_",
+            "args": "after-used",
+            "argsIgnorePattern": "^_",
+        },
+    ]
   },
 }
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,38 +1,54 @@
-import authController from "@server/controllers/auth.controller"
-import { ResponseStatusEnum } from "@server/types/enums/ResponseStatusEnum"
-import useAuthStore from "@src/stores/auth.store"
-import { User } from "firebase/auth"
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import authController from "@server/controllers/auth.controller";
+import { ResponseStatusEnum } from "@server/types/enums/ResponseStatusEnum";
+import useAuthStore from "@src/stores/auth.store";
+import { User } from "firebase/auth";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SignupPage = () => {
-    const authStore = useAuthStore()
-    const navigate = useNavigate()
-    const [credentials, setCredentials] = useState({
-        email: '',
-        password: '',
-        confirmPassword: ''
-    })
+  const authStore = useAuthStore();
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
 
-    const handleSignup = async () => {
-        const res = await authController.signUpWithEmailAndPassword(credentials.email, credentials.password, credentials.confirmPassword)
-        if (res.status === ResponseStatusEnum.SUCCESS) {
-            const user = res.body as User
-            authStore.setUser(user)
-            navigate('/')
-        } else {
-            alert(res.body)
-        }
+  const handleSignup = async () => {
+    const res = await authController.signUpWithEmailAndPassword(
+      credentials.email,
+      credentials.password,
+      credentials.confirmPassword
+    );
+    if (res.status === ResponseStatusEnum.SUCCESS) {
+      const user = res.body as User;
+      authStore.setUser(user);
+      navigate("/");
+    } else {
+      alert(res.body);
     }
+  };
 
-    return (
-        <div>
-            <input type="email" value={credentials.email} onChange={(e) => setCredentials({ ...credentials, email: e.target.value })} />
-            <input type="password" value={credentials.password} onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}  />
-            <input type="password" value={credentials.confirmPassword} onChange={(e) => setCredentials({ ...credentials, confirmPassword: e.target.value })}  />
-            <button onClick={handleSignup}>Sign Up</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <input
+        type="email"
+        value={credentials.email}
+        onChange={(e) => setCredentials({ ...credentials, email: e.target.value })}
+      />
+      <input
+        type="password"
+        value={credentials.password}
+        onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+      />
+      <input
+        type="password"
+        value={credentials.confirmPassword}
+        onChange={(e) => setCredentials({ ...credentials, confirmPassword: e.target.value })}
+      />
+      <button onClick={handleSignup}>Sign Up</button>
+    </div>
+  );
+};
 
-export default SignupPage
+export default SignupPage;
